1e860eefac45617fde1b7ecfdb9f7f4e55adbd27
Jasmine <wrkerr9@gmail.com>
2022-10-19 15:29:05 -0700

    Add 'edit comment' button to comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -11,3 +11,38 @@ function readmore(e) {
     restofcomment.classList.toggle('hidden');
 
 }
+
+function displayEditcommentForm(id){
+    console.log("Comment ID: %s", id)
+    const editCommentForm = document.getElementById("editComment" + id)
+    console.log(editCommentForm);
+    editCommentForm.classList.toggle('hidden');
+    const commentTextForm = document.getElementById('commentTextForm' + id)
+    console.log(commentTextForm.value.length)
+    
+    commentTextForm.setSelectionRange(commentTextForm.value.length, commentTextForm.value.length) // make user focus on end of comment.
+    commentTextForm.focus()// make user focus
+}
+function editComment(e) {
+    e.preventDefault()
+    // console.log("hello");
+    // console.log(e)
+    const form = e.target;
+    const formID = e.target.id
+    const commentID = e.target.dataset.commentid// formID.substr('commentTextForm'.length, formID.length);
+    // console.log("ID to be edited: %s", commentID)
+
+    const newCommentText = e.target.querySelector('input[type=text]').value
+    const commentSpanID = 'commentSpan' + commentID;
+    // console.log(commentSpanID);
+    const commentSpan = document.getElementById(commentSpanID)
+    // console.log(newCommentText)
+    // console.log(commentSpan)
+    commentSpan.innerText = newCommentText;
+    form.classList.toggle('hidden');              
+}
+function showLength(id) {
+    const commentTextForm = document.getElementById('commentTextForm' + id)
+
+    // console.log(commentTextForm.value.length)
+}
